fcb1b1b6406bd31bebfe981605bd750ff5f9712e
Minh Ha <nhat.minh.ha@gmail.com>
2013-08-09 23:30:06 +1200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Undecimo::Application.config.secret_token = '60c40c11f1c3294f2b7f8fbde2f75a87683d42793aad3c41cc592a76a1ba7e25e20a4602f1d09d76861e3bd1df653b6faf01f4fbc86e3ad6fd04fb86e33d2572'
